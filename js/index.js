a23c1fc10c849008d23161b508c97653f14a3883
Roderick Fitzgerald <liber6schol@outlook.com>
2024-12-16 16:13:36 -0500

    updated readme and experimented in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/*
+"use strict";
+
+let book = "Purity of Heart Is to Will One Thing";
+
+console.log(book);
+
+const PI = 3.14;
+
+console.log(PI);
+
+let author = "Soren Kierkegaard";
+
+console.log(author);
+
+book = "The Book of Five Rings";
+
+console.log(book);
+
+book = "The Underground History of American Education";
+
+console.log(book);
+
+const birthday = "03/06/1990";
+
+console.log(birthday);
+
+let date = "12/16/2024";
+
+console.log(date);
+
+let age = "34";
+
+console.log(age);
+*/
